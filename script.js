4b5bc66a7c3a8b2b9e74fc37a09585fa61d41fce
Fazliddin-04 <fazliddinpardayev004@gmail.com>
2021-11-07 08:21:24 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+// O'yinchi haqida
+var name = prompt('"Supper kalla" o`yiniga xush kelibsiz!\nIsmingizni kiring')
+var age = prompt("Yoshingizni ham kiriting")
+console.log("O'yinchi: " + name);
+console.log("O'yinchi yoshi: " + age + '\n ');
+
+// Misollar
+
+var lvl1 = parseInt(prompt('1 + 1 = ?', '2'))
+console.log("Level 1:\n 1 + 1 = " + (1 + 1));
+console.log('sizning javob: ' + lvl1 + '\n ');
+
+alert('Qiyin emasmi?')
+
+var lvl2 = +prompt('8987 + 1013 = ?')
+console.log("Level 2:\n 8987 + 1013 = " + (8987 + 1013));
+console.log('sizning javob: ' + lvl2 + '\n ');
+
+var lvl3 = +prompt('1234 - 567 = ?')
+console.log("Level 3:\n 1234 - 567 = " + (1234 - 567));
+console.log('sizning javob: ' + lvl3 + '\n ');
+
+var lvl4 = +prompt('11 * 11 = ?')
+console.log("Level 4:\n 11 * 11 = " + (11 * 11));
+console.log('sizning javob: ' + lvl4 + '\n ');
+
+var lvl5 = +prompt('144 / 12 = ?')
+console.log("Level 5:\n 144 / 12 = " + (144 / 12));
+console.log('sizning javob: ' + lvl5 + '\n ');
+
+var lvl6 = +prompt('"619 / 3" bo`lsa, qoldiq nechi qoladi?')
+console.log("Level 6:\n '619 / 3'ning qoldig\`i " + 619 % 3);
+console.log('sizning javob: ' + lvl6 + '\n ');
+
+var lvl7 = +prompt('2 ning 3-darajasi = ?')
+console.log("Level 7:\n 2 ning 3-darajasi" + (2 ** 3));
+console.log('sizning javob: ' + lvl7 + '\n ');
+
+var lvl8 = +prompt('x + 3 = 6, x = ?')
+console.log("Level 8:\n x + 3 = 6, x = " + (6 - 3));
+console.log('sizning javob: ' + lvl8 + '\n ');
+
+
+var lvl10 = +prompt('1/3 + 5/8 = ?')
+console.log("Level 100:\n 1/3 + 5/8 = " + ((1 / 3) + (5 / 8)));
+console.log('sizning javob: ' + lvl10 + '\n ');
+
+alert('F12 tugmasi bosing va konsol oynasi orqali natijalarni tekshiring')
